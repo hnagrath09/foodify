d88e437c0a8a60ed6a2b9185a0ce4d4335dcc8c7
Himanshu Nagrath <him.nagrath@gmail.com>
2021-05-04 09:14:14 +0530

    added stack navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import React, { useState } from "react"
-import { Text, View } from "react-native"
-import tailwind from "tailwind-rn"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import CreateAccount from "./components/create-account"
+import Login from "./components/login"
 import Onboard from "./components/onboard"
+import Home from "./components/home"
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  const [showOnboarding, setShowOnboarding] = useState(true)
+  const [showOnboarding, setShowOnboarding] = useState(false)
 
   function handleDone() {
     setShowOnboarding(false)
   }
 
   return showOnboarding ? (
-    <Onboard handleDone={handleDone} />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="onboarding">
+          {props => <Onboard {...props} handleDone={handleDone} />}
+        </Stack.Screen>
+        <Stack.Screen name="signup">
+          {props => <CreateAccount {...props} handleDone={handleDone} />}
+        </Stack.Screen>
+        <Stack.Screen name="login">
+          {props => <Login {...props} handleDone={handleDone} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   ) : (
-    <View style={tailwind("flex-1 items-center justify-center")}>
-      <Text>Welcome to Home Page!!!</Text>
-    </View>
+    <Home />
   )
 }
